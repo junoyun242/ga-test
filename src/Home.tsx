1b5c7055f60c6ee11863ef525aae4d69c1c9b053
Juno Yun <joyun@ywmobile.co.kr>
2023-05-08 14:33:58 +0900

    add react-ga4

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,31 @@
 import { useState } from "react";
+import ReactGA from "react-ga4";
 
 const Home = () => {
   const [count, setCount] = useState(0);
 
+  const onBtnClicked = (isPlus: boolean) => {
+    if (isPlus) {
+      setCount(count + 1);
+      ReactGA.event({
+        category: "clicked_plus",
+        action: "clicked_plus",
+        label: "clicked_plus", // optional
+      });
+    } else {
+      setCount(count - 1);
+      ReactGA.event({
+        category: "clicked_minus",
+        action: "clicked_minus",
+        label: "clicked_minus", // optional
+      });
+    }
+  };
   return (
     <div className="home-component">
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => onBtnClicked(true)}>-</button>
       <span>{count}</span>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => onBtnClicked(false)}>+</button>
     </div>
   );
 };
